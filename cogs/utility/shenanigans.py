f99d0a5a7db657b534d911e72eba768d82e30211
Lon-the-ham <116809168+Lon-the-ham@users.noreply.github.com>
2023-01-15 01:22:23 +0100

    Update shenanigans.py

--- cogs/utility/shenanigans.py
+++ cogs/utility/shenanigans.py
@@ -20,6 +20,21 @@ class Shenanigans(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    async def is_valid_server(ctx):
+        server = ctx.message.guild
+        if server is None:
+            raise commands.CheckFailure(f'Command does not work in DMs.')
+            return False
+        else:
+            valid_servers = [413011798552477716]
+            guild_id = server.id
+            print(guild_id)
+            if guild_id in valid_servers:
+                return True
+            else:
+                raise commands.CheckFailure(f'Command does only work on specific servers.')
+                return False
+
 
     ### avril fools
     @commands.command(name='mrec', aliases = ['memerec', 'memerecommendation'])
