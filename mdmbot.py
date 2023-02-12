8b0f231f80f686e1e63e821e1f82577958b6fbb2
Lon-the-ham <116809168+Lon-the-ham@users.noreply.github.com>
2023-02-12 23:58:19 +0100

    try catch in on_ready

--- mdmbot.py
+++ mdmbot.py
@@ -24,10 +24,7 @@ discord_token = config.discord_token
 
 
 class YataBot(commands.Bot):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
-        
-        #def __init__(self):
+    def __init__(self):
         super().__init__(
             command_prefix = '-',
             intents = discord.Intents.all(),
@@ -35,14 +32,15 @@ class YataBot(commands.Bot):
 
         self.initial_extensions = [
             "cogs.admin.servermod",
-            "cogs.utility.exchanges",
-            "cogs.utility.utility",
-            "cogs.utility.shenanigans",
             "cogs.backlog.memo",
             "cogs.pingterest.pingterest",
+            "cogs.reactionevents.reactionevents",
             "cogs.roles.roles",
             "cogs.roles.reactionroles",
-            "cogs.reactionevents.reactionevents"
+            "cogs.settings.settings",
+            "cogs.utility.exchanges",
+            "cogs.utility.utility",
+            "cogs.utility.shenanigans"
         ]
 
     async def setup_hook(self):
@@ -53,65 +51,71 @@ class YataBot(commands.Bot):
 
     async def on_ready(self):
         print('logged in as {0.user}'.format(bot))
-        channel = bot.get_channel(416384984597790750)
-        await channel.send(f'`I haveth logged in` <:smug:955227749415550996>')
 
-
-
-
-        # set default settings !
         try:
-            settings = []
-            print('+++ settings: +++')
-            with open('settings/default_settings.txt', 'r') as s:
-                for line in s:
-                    print(line.strip())
-                    settings.append(line.strip())
-            print('--- ---')
+            channel = bot.get_channel(416384984597790750)
+            await channel.send(f'`I haveth logged in` <:smug:955227749415550996>')
 
-            for s in settings:
-                if ":" in s:
-                    parameter = s.split(":",1)[0].strip().lower()
-                    value = s.split(":",1)[1].strip()
 
-                    ### PARAMETER BOT STATUS
-                    if parameter in ['status']:
-                        if ' ' in value:
-                            stat_type = value.split(":",1)[0].lower().strip()
-                            stat_name = value.split(":",1)[1].strip()
-                            if stat_type in ['p', 's', 'l', 'w', 'n']:
-                                if stat_type == 'p':
-                                    await self.bot.change_presence(activity=discord.Game(name=stat_name))
-                                    print(f'set status PLAYING {stat_name}')
-                                elif stat_type == 's':
-                                    my_twitch_url = "https://www.twitch.tv/mdmbot/home"
-                                    await self.bot.change_presence(activity=discord.Streaming(name=stat_name, url=my_twitch_url))
-                                    print(f'set status STREAMING {stat_name}')
-                                elif stat_type == 'l':
-                                    await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=stat_name))
-                                    print(f'set status LISTENING TO {stat_name}')
-                                elif stat_type == 'w':
-                                    await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=stat_name))
-                                    print(f'set status WATCHING {stat_name}')
-                                elif stat_type == 'n':
-                                    await self.bot.change_presence(activity=None)
-                                    print('empty status')
+
+
+
+            # set default settings !
+            try:
+                settings = []
+                print('+++ settings: +++')
+                with open('cogs/settings/default_settings.txt', 'r') as s:
+                    for line in s:
+                        print(line.strip())
+                        settings.append(line.strip())
+                print('--- ---')
+
+                for s in settings:
+                    if ":" in s:
+                        parameter = s.split(":",1)[0].strip().lower()
+                        value = s.split(":",1)[1].strip()
+
+                        ### PARAMETER BOT STATUS
+                        if parameter in ['status']:
+                            if ' ' in value:
+                                stat_type = value.split(" ",1)[0].lower().strip()
+                                stat_name = value.split(" ",1)[1].strip()
+                                if stat_type in ['p', 's', 'l', 'w', 'n']:
+                                    if stat_type == 'p':
+                                        await self.change_presence(activity=discord.Game(name=stat_name))
+                                        print(f'set status PLAYING {stat_name}')
+                                    elif stat_type == 's':
+                                        my_twitch_url = "https://www.twitch.tv/mdmbot/home"
+                                        await self.change_presence(activity=discord.Streaming(name=stat_name, url=my_twitch_url))
+                                        print(f'set status STREAMING {stat_name}')
+                                    elif stat_type == 'l':
+                                        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=stat_name))
+                                        print(f'set status LISTENING TO {stat_name}')
+                                    elif stat_type == 'w':
+                                        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=stat_name))
+                                        print(f'set status WATCHING {stat_name}')
+                                    elif stat_type == 'n':
+                                        await self.change_presence(activity=None)
+                                        print('empty status')
+                                else:
+                                    print('first argument was not a valid status type, setting status type WATCHING')
+                                    await self.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=value))
+                            elif value in ['', 'n', 'N']:
+                                await self.change_presence(activity=None)
+                                print('no status set')
                             else:
-                                print('first argument was not a valid status type, setting status type WATCHING')
-                                await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=value))
-                        elif value in ['', 'n', 'N']:
-                            await self.bot.change_presence(activity=None)
-                            print('no status set')
-                        else:
-                            print('>> could not determine status type -.-')
-                            await self.bot.change_presence(activity=None)
+                                print('>> could not determine status type -.-')
+                                await self.change_presence(activity=None)
 
-                    ### 
-                else:
-                    print(f'cannot parse parameter and value from: {s}')
-        except Exception as e:
-            print(e)
-            await channel.send(f'Error in loading default settings <:nervous:975219600272801802>')
+                        ### 
+                    else:
+                        print(f'cannot parse parameter and value from: {s}')
+            except Exception as e:
+                print(e)
+                await channel.send(f'Error in loading default settings <:nervous:975219600272801802>')
+        
+        except:
+            print("error in executing on_ready")
 
 
 bot = YataBot()
