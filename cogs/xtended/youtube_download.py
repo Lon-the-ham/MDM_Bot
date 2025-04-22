eeb8040d4c5d1af8a00b1666768fc3b06fede904
Lon-the-ham <116809168+Lon-the-ham@users.noreply.github.com>
2025-04-22 23:51:23 +0200

    Update youtube_download.py
    youtube audio download

--- cogs/xtended/youtube_download.py
+++ cogs/xtended/youtube_download.py
@@ -55,12 +55,27 @@ class YouTube_Download(commands.Cog):
 
         return title
 
+
+
+    @to_thread
+    def downloadYouTubeAudio(self, videourl_list, ydl_opts, filename):
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(videourl_list[0], download=False)
+            try:
+                title = ydl.sanitize_info(info)['title']
+            except Exception as e:
+                print("Error:", e)
+                title = "<youtube_audio>"
+            ydl.download(videourl_list)
+
+        return title
+
         
 
     @commands.command(name='ytdl', aliases = ['youtubedownload'])
     @commands.check(YT_Check.is_youtubedownload_enabled)
     #@commands.has_permissions(manage_guild=True)
-    @commands.check(util.is_main_server)
+    #@commands.check(util.is_main_server)
     @commands.check(util.is_active)
     async def _youtube_download(self, ctx: commands.Context, *args):
         """㊙️ Downloads YouTube video
@@ -134,6 +149,88 @@ class YouTube_Download(commands.Cog):
     @_youtube_download.error
     async def youtube_download_error(self, ctx, error):
         await util.error_handling(ctx, error)
+
+
+
+    @commands.command(name='ytmp3', aliases = ['yta', 'yttmp3', 'youtubeaudio', 'youtubetomp3', 'youtube2mp3', 'yt2mp3'])
+    @commands.check(YT_Check.is_youtubedownload_enabled)
+    #@commands.has_permissions(manage_guild=True)
+    #@commands.check(util.is_main_server)
+    @commands.check(util.is_active)
+    async def _youtube_audioextract(self, ctx: commands.Context, *args):
+        """㊙️ Extracts YouTube audio
+        """
+
+        if len(args) == 0:
+            await ctx.send("Command needs youtube url as argument.")
+            return
+
+        emoji = util.emoji("load")
+
+        try:
+            await ctx.message.add_reaction(emoji)
+        except:
+            pass
+
+        try:
+            userid   = ctx.message.author.id
+            now      = int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds())
+
+            videourl_list = []
+
+            for arg in args:
+                url = arg.replace("<","").replace(">","").replace(",","").strip()
+                if url == "":
+                    continue
+
+                videourl_list.append(url)
+                break # download only one file
+
+            if len(videourl_list) == 0:
+                await ctx.send("Arguments must be URLs.")
+                return
+
+            filename = f'yt_{userid}_{now}.mp3'
+
+            ydl_opts = {
+                'extract_audio': True,
+                'format': 'bestaudio',
+                'paths': {'home': './temp', 'temp': './temp'},
+                'outtmpl': {'default': filename}
+            }
+
+            title = await self.downloadYouTubeAudio(videourl_list, ydl_opts, filename)
+
+            try:
+                title_clean = util.cleantext(title)
+                caption = f"Downloaded `{title_clean}` (audio only):"
+                print("sending file...")
+                await ctx.reply(caption[:2000], file=discord.File(rf"temp/{filename}"))
+            except Exception as e:
+                message = f"Error while trying to send YouTube audio:\n{e}"
+                await ctx.send(message[:2000])
+
+            try:
+                os.rmdir("temp/temp")
+            except:
+                pass
+            os.remove(f"temp/{filename}")
+
+        except Exception as e:
+            message = f"Error while trying to download YouTube audio:\n{e}"
+            await ctx.send(message[:2000])
+
+        try:
+            mdmbot_id = int(self.bot.application_id)
+            mdmbot = ctx.guild.get_member(mdmbot_id)
+            await ctx.message.remove_reaction(emoji, mdmbot)
+        except Exception as e:
+            print("Error:", e)
+    
+    @_youtube_audioextract.error
+    async def youtube_audioextract_error(self, ctx, error):
+        await util.error_handling(ctx, error)
+
 
 
 async def setup(bot: commands.bot) -> None:
